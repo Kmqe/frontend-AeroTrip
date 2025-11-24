ff4a4ac23e7b83db84fff989da4200c126f976f3
kmqe <7moodi115b2@gmail.com>
2025-11-24 15:15:19 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import Pages from "./components/pages/Pages";
+import "./index.css";
+import "./fonts.css";
+
+console.log("Hi");
 const App = () => {
-  return <div>App</div>;
+  return <Pages />;
 };
 
 export default App;
